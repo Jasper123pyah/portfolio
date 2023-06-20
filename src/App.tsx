be4d26aadef63a125a3f858c56ccf4950d631c5e
jasper123pyah <jasper.vanden.meiracker@indicia.nl>
2023-06-20 21:09:05 +0200

    Setup of css

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import './App.css'
 
 function App() {
 
   return (
-    <>
-    </>
+    <h1>
+      Whereas recognition of the inherent dignity and of the equal and inalienable rights of all members of the human family is the foundation of freedom, justice and peace in the world,
+    </h1>
   )
 }
 
